960488f801426689a4da32fa07eef3ac0a232755
iStarzG <driftcd@mail.ru>
2024-04-04 01:50:19 +0500

    Fix commit

--- src/test/java/config/DriverConfig.java
+++ src/test/java/config/DriverConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/driverBrowserStack.properties"})
+        "classpath:config/driver.properties"})
 
 public interface DriverConfig extends Config {
 
